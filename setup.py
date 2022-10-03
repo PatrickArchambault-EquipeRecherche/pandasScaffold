24a1a9a2a323aa97c131eb76813b8ed32cd08a99
William Witteman <william.witteman.cisssca@ssss.gouv.qc.ca>
2022-10-03 10:41:27 -0400

    Module packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name='scaffold-pandas',
+      version='0.1',
+      description='Helpful functions for pandas',
+      url='https://github.com/PatrickArchambault-EquipeRecherche/scaffold-pandas',
+      author='William Witteman',
+      author_email='william@witteman.ca',
+      license='GPL3',
+      packages=['scaffold-pandas','splitCSV'],
+      zip_safe=False)
