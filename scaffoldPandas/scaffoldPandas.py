6f93dc760c35db85f704b05592a5c9492bd3c907
PatrickArchambault-EquipeRecherche <83590296+PatrickArchambault-EquipeRecherche@users.noreply.github.com>
2023-03-03 16:05:48 -0500

    Adding more patterns

--- scaffoldPandas/scaffoldPandas.py
+++ scaffoldPandas/scaffoldPandas.py
@@ -296,18 +296,46 @@ def inspectColumn( columnname , printout=True ):
 #newDataFrame = pandas.concat( [firstDataFrame , firstDataFrame] ).drop_duplicates()
 #
 #
+## Connect two dataframes that *don't* overlap, using either merge or concat
+#
+## concat requires implicit connecion criteria
+#newdataframe = pandas.concat([onedf, otherdf], axis=1, join="inner")
+#
+## merge requires explicit connection criteria
+#newdataframe = pandas.merge(onedf, otherdf, on=CONNECTIONCOLUMN)
+#
+#
 ## Change the encoding of NotANumber on import - useful when your data has 
 ## inadvertent entries that are not null, like sodium (NA)
 #from pandas._libs.parsers import STR_NA_VALUES
 #
+## NOTE: STR_NA_VALUES is a set, so you can use set operations on it (add(), remove(), clear())
+#
 #disable_na_values = { "NA" }
-#my_default_na_values = STR_NA_VALUES - disable_na_values
+#my_default_na_values = STR_NA_VALUES.remove(disable_na_values)
 #df = pandas.read_csv( "myCSVfile.csv" , na_values = my_default_na_values )
 #
 #
 ## Pad out values with 4 leading zeros on pandas.read_csv()
 #df = pandas.read_csv("myCSVfile.csv", converters={'UnpaddedColumn1': '{:0>4}'.format, 'UnpaddedColumn2': '{:0>4}'.format}) 
 #
+#
+## Put all information for a given ID from multiple rows to a single row
+#
+#raw_data = {'patient': [1, 1, 1, 2, 2],
+#                'obs': [1, 2, 3, 1, 2],
+#          'treatment': [0, 1, 0, 1, 0],
+#              'score': [6252, 24243, 2345, 2342, 23525],
+#            'onetime': ["thing",pandas.NA,pandas.NA,"stuff",pandas.NA]}
+#
+#topivot = pandas.DataFrame(raw_data, columns = ['patient', 'obs', 'treatment', 'score', 'onetime'])
+#topivot.head
+#pivoted = topivot.pivot(index='patient', columns='obs', values='score')
+#pivoted.head
+#
+## Propogate the first value down a column based on another column value (ID) 
+## (push demographic data for person X to all person X rows).
+#df.columnToFill = df.groupby("ID").columnToFill.transform("first")
 #
 ## Filter a dataframe into a subset that all meets a criteria.
 ## In the example case, we use pandas.factorize() to get all of the values, 
@@ -318,4 +346,18 @@ def inspectColumn( columnname , printout=True ):
 #pos = ['DÉTECTÉ', 'DÉTEDÉTECTÉ', 'detecté', 'detecte', '03DÉTECTÉ']
 #
 #posCases = firstDataFrame[firstDataFrame["RESULTAT"].isin(pos)]
-
+#
+#
+## Take a series of rows that are sparse but related rows with an identifying 
+## column, and turn them into a "flat" dataframe where there is just one row 
+## per identifier. Look at "aggregate" functions.
+#
+#df = pandas.DataFrame([{"id":0,"a":1,"b":pandas.NA,"c":pandas.NA,},{"id":0,"a":pandas.NA,"b":1,"c":pandas.NA,},{"id":0,"a":pandas.NA,"b":pandas.NA,"c":1,},{"id":0,"a":pandas.NA,"b":pandas.NA,"c":pandas.NA,}])
+#
+#flatDF = df.groupby("id").first()
+#
+#
+## If you have accents in your column headings, it can be a PITA to type and
+## use. You can extract the column headings into a list, and then replace then
+## by index, preventing you from typing anything difficult.
+# df.columns.values[2] = "newname"
