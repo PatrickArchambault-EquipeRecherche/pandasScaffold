5f995c0827856e14b459385e4d8d5b6df5db7471
PatrickArchambault-EquipeRecherche <83590296+PatrickArchambault-EquipeRecherche@users.noreply.github.com>
2023-04-11 14:58:50 -0400

    Like https://pypi.org/project/Example-Project/

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-#!/usr/bin/python
+from setuptools import setup, find_packages
+import os
 
-from setuptools import setup
+current_folder = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_folder, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
 
-setup(name='scaffoldPandas',
-      version='0.16',
-      description='Helpful functions for pandas',
-      url='https://github.com/PatrickArchambault-EquipeRecherche/scaffold-pandas',
-      author='William Witteman',
-      author_email='william@witteman.ca',
-      license='GPL3',
-      packages=['scaffoldPandas'],
-      zip_safe=False)
+setup(
+  name='scaffoldPandas',
+  version='0.16',
+  description='Helpful functions for pandas',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/PatrickArchambault-EquipeRecherche/scaffold-pandas',
+  author='William Witteman',
+  author_email='william@witteman.ca',
+  license='GPL3',
+  packages=find_packages(exclude='tests'),
+  python_requires='>=3.7',
+)
